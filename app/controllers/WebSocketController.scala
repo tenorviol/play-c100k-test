fb2bbf4dcfb264cc5a002ee1411109e4679c4b16
Chris Johnson <chris@eero.com>
2017-06-02 17:46:20 -0700

    Reimplemented the echo web socket to use an actor

--- app/controllers/WebSocketController.scala
+++ app/controllers/WebSocketController.scala
@@ -1,20 +1,42 @@
 package controllers
 
-import akka.NotUsed
-import akka.stream.scaladsl.Flow
+import javax.inject._
+
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import akka.stream.Materializer
 import play.api.Logger
+import play.api.libs.streams.ActorFlow
 import play.api.mvc.{Controller, WebSocket}
 
-import scala.concurrent.Future
+import scala.concurrent.ExecutionContext
 
-class WebSocketController extends Controller {
-  private val log = Logger(getClass)
+object WebSocketController {
 
-  def wsEcho: WebSocket = WebSocket.acceptOrResult[String, String] { request =>
-    val echoFlow: Flow[String, String, NotUsed] = Flow[String]
-    Future.successful(
-      Right(echoFlow)
-    )
+  object WebSocketEchoActor {
+    def props(out: ActorRef) = Props(classOf[WebSocketEchoActor], out)
   }
 
+  class WebSocketEchoActor(out: ActorRef) extends Actor {
+    def receive: Receive = {
+      case m: String =>
+        // echo
+        out ! m
+    }
+
+    override def postStop(): Unit = {
+      println("Finished!!!")
+    }
+  }
+
+}
+
+@Singleton
+class WebSocketController @Inject() (implicit system: ActorSystem, materializer: Materializer, ec: ExecutionContext) extends Controller {
+  import WebSocketController._
+
+  private val log = Logger(getClass)
+
+  def wsEcho: WebSocket = WebSocket.accept[String, String] { request =>
+    ActorFlow.actorRef(out => WebSocketEchoActor.props(out))
+  }
 }
