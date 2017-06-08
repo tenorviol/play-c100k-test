7d53afa00e5aee87e7cdc1cb873ce21e667ba68e
Chris Johnson <chris@eero.com>
2017-06-08 11:18:11 -0700

    Modified the server websocket controller to not use an actor

--- app/server/controllers/WebSocketController.scala
+++ app/server/controllers/WebSocketController.scala
@@ -2,48 +2,26 @@ package server.controllers
 
 import javax.inject._
 
-import akka.actor.{Actor, ActorRef, ActorSystem, Props}
-import akka.stream.Materializer
-import play.api.Logger
-import play.api.libs.streams.ActorFlow
+import akka.actor.ActorSystem
+import akka.stream.scaladsl.Flow
 import play.api.mvc.{Controller, WebSocket}
 import server.ServerMetrics
 
 import scala.concurrent.ExecutionContext
 
-object WebSocketController {
-
-  object WebSocketEchoActor {
-    def props(out: ActorRef, metrics: ServerMetrics): Props = {
-      Props(classOf[WebSocketEchoActor], out, metrics)
-    }
-  }
-
-  class WebSocketEchoActor(out: ActorRef, metrics: ServerMetrics) extends Actor {
-    metrics.connections.increment()
-
-    def receive: Receive = {
-      case m: String =>
-        // echo
-        out ! m
-    }
-
-    override def postStop(): Unit = {
-      metrics.connections.decrement()
-    }
-  }
-
-}
-
 @Singleton
 class WebSocketController @Inject() (
   metrics: ServerMetrics
-)(implicit system: ActorSystem, materializer: Materializer, ec: ExecutionContext) extends Controller {
-  import WebSocketController._
-
-  private val log = Logger(getClass)
+)(implicit system: ActorSystem, ec: ExecutionContext) extends Controller {
 
   def wsEcho: WebSocket = WebSocket.accept[String, String] { request =>
-    ActorFlow.actorRef(out => WebSocketEchoActor.props(out, metrics))
+    Flow[String].watchTermination() { (mat, doneFuture) =>
+      metrics.connections.increment()
+      doneFuture.onComplete { _ =>
+        metrics.connections.decrement()
+      }
+      mat
+    }
   }
+
 }
